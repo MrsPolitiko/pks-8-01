02b37718e9257f04d88e17173922370501a085af
politikoks <politiko-ks@narod.ru>
2024-10-07 21:30:16 +0300

    Capitalize first letters every word

--- hello.py
+++ hello.py
@@ -1,2 +1,12 @@
 import sys
-sys.stdout.write("Hello World\n")
+
+def get_vowels(String):
+    return [each for each in String if each in "aeiou"]
+
+def capitalize(String):
+    return String.title()
+
+
+sys.stdout.write(capitalize("hello world\n"))
+
+
